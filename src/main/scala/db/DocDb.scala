18ee39e2aa1938a59e92ec18296c5d1bad726a63
S.H.Ayat <fahim.ayat@gmail.com>
2021-11-29 13:40:59 +0330

    get by key

--- src/main/scala/db/DocDb.scala
+++ src/main/scala/db/DocDb.scala
@@ -9,6 +9,7 @@ import java.io.FileInputStream
 import java.nio.charset.StandardCharsets
 
 case class Doc(key: String, value: String)
+
 trait DocDb {
   def append(collection: String, doc: Doc): Unit
 
@@ -46,43 +47,44 @@ object DocDb extends DocDb {
       collection: String,
       doc: Doc
   ): Unit = {
-    import doc.{key, value}
-    val writer = new FileWriter(getCollectionFile(collection), true)
-    val keyArray = key.toCharArray()
-    val valueArray = value.toCharArray()
-    writer.write(keyArray.length)
-    writer.write(valueArray.length)
-    writer.write(key.toCharArray())
-    writer.write(value.toCharArray())
-    writer.close()
+    collection.synchronized {
+      import doc.{key, value}
+      val writer = new FileWriter(getCollectionFile(collection), true)
+      val keyArray = key.toCharArray()
+      val valueArray = value.toCharArray()
+      writer.write(keyArray.length)
+      writer.write(valueArray.length)
+      writer.write(key.toCharArray())
+      writer.write(value.toCharArray())
+      writer.close()
+    }
   }
 
   override def getByKey(
       collection: String,
       key: String
-  ): Option[Doc] = ???
+  ): Option[Doc] = getAll(collection).find(_.key == key)
 
   override def getAll(collection: String): List[Doc] = {
-    val reader = new FileInputStream(getCollectionFile(collection))
-
-    def readSample(r: FileInputStream): Doc = {
-      val keyLength = r.read()
-      val valueLength = r.read()
-      val keyArray = r.readNBytes(keyLength)
-      val valueArray = r.readNBytes(valueLength)
-      val key = new String(keyArray, StandardCharsets.UTF_8);
-      val value = new String(valueArray, StandardCharsets.UTF_8);
-      Doc(key, value)
+    collection.synchronized {
+      val reader = new FileInputStream(getCollectionFile(collection))
+      def readSample(r: FileInputStream): Doc = {
+        val keyLength = r.read()
+        val valueLength = r.read()
+        val keyArray = r.readNBytes(keyLength)
+        val valueArray = r.readNBytes(valueLength)
+        val key = new String(keyArray, StandardCharsets.UTF_8);
+        val value = new String(valueArray, StandardCharsets.UTF_8);
+        Doc(key, value)
+      }
+      val data = List.unfold[Doc, FileInputStream](reader) { r =>
+        if (r.available() > 0)
+          Some(readSample(r) -> r)
+        else
+          None
+      }
+      reader.close()
+      data
     }
-    val data = List.unfold[Doc, FileInputStream](reader) { r =>
-      if (r.available() > 0)
-        Some(readSample(r) -> r)
-      else
-        None
-    }
-    reader.close()
-
-    data
   }
-
 }
--- src/test/scala/db/DocDbTest.scala
+++ src/test/scala/db/DocDbTest.scala
@@ -1,28 +1,75 @@
 package db
 
+import scala.concurrent.{Future, ExecutionContext, duration}
+import ExecutionContext.Implicits.global
+import duration._
+
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.BeforeAndAfter
 import org.scalatestplus.scalacheck.ScalaCheckDrivenPropertyChecks._
 import org.scalacheck.Gen
+import scala.concurrent.Await
 
 class DocDbTest extends AnyFlatSpec with Matchers with BeforeAndAfter {
+  private implicit val generatorDrivenConfig =
+    PropertyCheckConfiguration(minSuccessful = 500)
 
   val docGen = for {
-    key <- Gen.asciiStr
-    value <- Gen.asciiStr
+    key <- Gen.alphaStr
+    value <- Gen.alphaStr
   } yield Doc(key, value)
 
   val docsGen = Gen.listOf(docGen)
 
-  "db" should "Work" in {
+  "db" should "append and retrieve multiple rows" in {
     forAll(docsGen) { docList =>
-      println(docList.length)
       DocDb.cleanup("test")
       docList.foreach(DocDb.append("test", _))
       val all = DocDb.getAll("test")
 
       docList shouldBe all
+    }
+  }
+
+  it should "apped and get all in parallel" in {
+    forAll(docsGen) { docList =>
+      val collectionName = "test2"
+      DocDb.cleanup(collectionName)
+
+      val futures = docList.map { doc =>
+        Future {
+          DocDb.append(collectionName, doc)
+        }
+      }
+      Await.result(Future.sequence(futures), 2.minutes)
+
+      val all = DocDb.getAll(collectionName)
+
+      docList.toSet shouldBe all.toSet
+    }
+  }
+
+  it should "getby key" in {
+    forAll(docsGen) { rawDocList =>
+      val docList = rawDocList.distinctBy(_.key)
+
+      val keys = docList.map(_.key).toSet
+      val collectionName = "test2"
+      DocDb.cleanup(collectionName)
+
+      docList.foreach(DocDb.append("test2", _))
+      val all = DocDb.getAll(collectionName)
+
+      docList
+        .map(_.key)
+        .flatMap(key => DocDb.getByKey(collectionName, key)) shouldBe docList
+
+      forAll("randomKey") { randomKey: String =>
+        whenever(!keys.contains(randomKey)) {
+          DocDb.getByKey(collectionName, randomKey) shouldBe None
+        }
+      }
     }
   }
 }
